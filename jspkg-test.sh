208435523ee704b0099cc610e0951769474e9838
Steve Schwartz <steve@alfajango.com>
2012-01-14 17:53:10 -0500

    Extract and unit-test download

--- jspkg-test.sh
+++ jspkg-test.sh
@@ -13,3 +13,11 @@ it_replaces_special_characters_in_name() {
   permalink="$($jspkg permalink easy\ tabs\ project)"
   test $permalink = 'easy-tabs-project'
 }
+
+it_downloads_zip() {
+  download="$($jspkg download easytabs)"
+  test "$download" = 'downloading easytabs'
+  exists="$([ -e ./easytabs.zip ] && echo yes || echo no)"
+  test $exists = 'yes'
+  rm ./easytabs.zip
+}
